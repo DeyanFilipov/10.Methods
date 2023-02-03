ca7c99a82fc399af06cec2496c84d4800f44d833
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-02-03 23:04:23 +0200

    Commit

---
+++ Exercise/CharactersInRange_03.java
@@ -0,0 +1,33 @@
+package Methods.Exercise;
+
+import java.util.Scanner;
+
+public class CharactersInRange_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String first = scanner.nextLine();
+        String second = scanner.nextLine();
+        char firstChar = first.charAt(0);
+        char secondChar = second.charAt(0);
+
+        charSequence(firstChar, secondChar);
+    }
+    public static void charSequence(char first, char second) {
+        while (first != second) {
+            if (second < first) {
+                second++;
+                if (second == first) {
+                    break;
+                }
+                System.out.print(second + " ");
+            } else {
+                first++;
+                if (first == second) {
+                    break;
+                }
+                System.out.print(first + " ");
+            }
+        }
+    }
+}
